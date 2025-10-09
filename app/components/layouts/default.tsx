378401b84f598b8ef24e2b424a6e1ebc1545b9da
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2025-10-09 15:09:55 +0700

    Refactor Home component to lazy load Recap and remove clientLoader function

--- app/components/layouts/default.tsx
+++ app/components/layouts/default.tsx
@@ -2,14 +2,6 @@ import { Outlet } from "react-router"
 import Navbar from "../navbar/navbar"
 import Footer from "../footer"
 
-export function HydrateFallback() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="border-4 border-primary border-t-transparent border-solid rounded-full w-16 h-16 animate-spin"></div>
-        </div>
-    );
-}
-
 export default () => {
     return (
         <>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,19 +3,14 @@ import Hero from "~/components/home-sections/hero";
 import Articles from "~/components/home-sections/articles";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { lazy, useEffect } from "react";
+import { lazy, useEffect, useState } from "react";
 import InfinitePhoto from "~/components/home-sections/infinite-photo";
-import type { Route } from "./+types/home";
 
-export const clientLoader = () => {
-  return { Recap: lazy(() => import("~/components/home-sections/recap")) };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-
-  const { Recap } = loaderData;
+export default function Home() {
+  const [Recap, setRecap] = useState<React.LazyExoticComponent<React.FC> | null>(null);
 
   useEffect(() => {
+    setRecap(lazy(() => import("~/components/home-sections/recap")));
     AOS.init({
       duration: 800, // Durasi animasi dalam milidetik
       once: true, // Animasi hanya terjadi sekali saat elemen muncul
@@ -27,7 +22,7 @@ export default function Home({ loaderData }: Route.ComponentProps) {
       <Hero />
       <InfinitePhoto />
       <About />
-      <Recap />
+      {Recap && <Recap />}
       <Articles />
     </>
   );
